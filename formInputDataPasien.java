3049982f03ed1935e6e893d97ff9a1a3f3b24d86
mrfauzii <reishifauzi24@gmail.com>
2023-09-16 13:28:53 +0700

    Menambahkan inputan data untuk data pasien

--- formInputDataPasien.java
+++ formInputDataPasien.java
@@ -19,6 +19,26 @@ public class formInputDataPasien {
         System.out.println("Masukkan Data Pasien");
         System.out.println("");
 
+        //membuat inputan data pasien untuk admin
+        System.out.println("No KTP                      :");
+        noKtp = sc.nextLine();
+        System.out.println("NO BPJS (Jika ada)          :");
+        noBpjs = sc.nextLine();
+        System.out.println("Nama                        :");
+        namaPasien = sc.nextLine();
+        System.out.println("Usia                        :");
+        usiaPasien = sc.nextInt();
+        KeluhanPasien = sc.nextLine();
+        System.out.println("Keluhan                     :");
+        KeluhanPasien = sc.nextLine();
+    
+        System.out.println("--------------------------------------------------");
+        
+        System.out.println("Potongan BPJS               :");
+        potonganBpjs = sc.nextInt();
+        System.out.println("Harga                       :");
+        harga = sc.nextInt();
+
         
         
     }
