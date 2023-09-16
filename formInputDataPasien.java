ea686c1c40a0e9728e49486d7e2ef219dd612d91
revaniputeri <revaninandaputeri@gmail.com>
2023-09-16 11:22:33 +0700

    Revani : Membuat variable yang di perlukan dan opening form

---
+++ formInputDataPasien.java
@@ -0,0 +1,26 @@
+import java.util.Scanner;
+public class formInputDataPasien {
+    public static void main (String[]args){
+        Scanner sc = new Scanner (System.in);
+
+        //membuat variable sting atau karakter
+        String noKtp;
+        String noBpjs;
+        String namaPasien;
+        String KeluhanPasien;
+        String usiaPasien;
+
+        //membuat varible int berupa angka yang memerlukan perhitungan
+        
+        int potonganBpjs;
+        int harga;
+        int hargaAkhir;
+
+        System.out.println("Selamat Datang Di Rumah Sakit X - Admin");
+        System.out.println("Masukkan Data Pasien");
+        System.out.println("");
+        
+        
+        
+    }
+}
