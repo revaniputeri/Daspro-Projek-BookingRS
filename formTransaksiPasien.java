7d3f6c05141fdec98dcf479b6b1fc4db02b2f9a8
fransiskawidyak <fransiskawidyak17@gmail.com>
2023-10-02 20:27:03 +0700

    menggabungkan file

--- formTransaksiPasien.java
+++ formTransaksiPasien.java
@@ -58,6 +58,47 @@ public class formTransaksiPasien {
         KeluhanPasien = sc.nextLine();
         System.out.print("Keluhan                     : ");
         KeluhanPasien = sc.nextLine();
+        System.out.println("==============Daftar Dokter=============");
+        System.out.println("| 1. Dokter Umum                        |"); 
+        System.out.println("| 2. Dokter Gigi                        |"); 
+        System.out.println("| 3. Bidan                              |");
+        System.out.println("| 4. perawat                            |"); 
+        System.out.println("| 5. petugas lab                        |"); 
+        System.out.println("| 6. tenaga gizi                        |"); 
+        System.out.println("Pilih nomor dokter yang dibutuhkan pasien : ");
+        int pemilihan = sc.nextInt();
+        String keluhan;
+
+        switch (pemilihan) {
+            case 1:
+                keluhan = "Keluhan Dokter Umum";
+                System.out.println("Dokter Umum - Konsultansi " + keluhan);
+                break;
+            case 2:
+                keluhan = "Keluhan Dokter Gigi";
+                System.out.println("Dokter Gigi - Konsultansi " + keluhan);
+                break;
+            case 3:
+                keluhan = "Keluhan Bidan";
+                System.out.println("Bidan - Konsultansi " + keluhan);
+                break;
+            case 4:
+                keluhan = "Keluhan Perawat";
+                System.out.println("Perawat - Konsultansi " + keluhan);
+                break;
+            case 5:
+                keluhan = "Keluhan Petugas Lab";
+                System.out.println("Petugas Lab - Konsultansi " + keluhan);
+                break;
+            case 6:
+                keluhan = "Keluhan Tenaga Gizi";
+                System.out.println("Tenaga Gizi - Konsultansi " + keluhan);
+                break;
+
+            default:
+                System.out.println("Inputan tidak valid!");
+                break;
+        }
         System.out.println("--------------------------------------------------");
 
         //potongan BPJS pada puskesmas 100%
